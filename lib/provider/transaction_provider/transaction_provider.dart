76383b6043f0678f5d16a49d44b749f652b605bb
Sandeep Abraham <sandeep.pggroup@gmail.com>
2023-08-08 14:51:26 +0530

    before transaction changed to provider

--- lib/provider/transaction_provider/transaction_provider.dart
+++ lib/provider/transaction_provider/transaction_provider.dart
@@ -1,3 +1,79 @@
 import 'package:flutter/foundation.dart';
+import 'package:hive_flutter/adapters.dart';
 
-class TransactionProvider with ChangeNotifier {}
+import '../../Account/balance.dart';
+import '../../models/category/category_model.dart';
+import '../../models/transaction/transaction_model.dart';
+
+const TRANSACTION_DB_NAME = 'transaction-db';
+
+class TransactionProvider with ChangeNotifier {
+  List<TransactionModel> transactionList = [];
+
+  List<TransactionModel> incomeListenable = [];
+  List<TransactionModel> expenseListenable = [];
+  List<TransactionModel> transationAll = [];
+
+  @override
+  Future<void> addTransaction(TransactionModel obj) async {
+    final transactionDB =
+        await Hive.openBox<TransactionModel>(TRANSACTION_DB_NAME);
+    transactionDB.put(obj.id, obj);
+
+    refresh();
+  }
+
+  Future<void> refresh() async {
+    final list = await getAllTransaction();
+    incomeListenable.clear();
+    expenseListenable.clear();
+    transationAll.clear();
+    await Future.forEach(list, (TransactionModel transation) {
+      if (transation.category.type == CategoryType.income) {
+        incomeListenable.add(transation);
+      } else if (transation.category.type == CategoryType.expense) {
+        expenseListenable.add(transation);
+      }
+    });
+
+    list.sort((first, second) => second.date.compareTo(first.date));
+    transactionList.clear();
+    transactionList.addAll(list);
+    notifyListeners();
+    balanceAmount();
+  }
+
+  @override
+  Future<List<TransactionModel>> getAllTransaction() async {
+    final transactionDB =
+        await Hive.openBox<TransactionModel>(TRANSACTION_DB_NAME);
+
+    return transactionDB.values.toList();
+  }
+
+  @override
+  Future<void> deleteTransaction(String id) async {
+    final transactionDB =
+        await Hive.openBox<TransactionModel>(TRANSACTION_DB_NAME);
+    await transactionDB.delete(id);
+    refresh();
+  }
+
+  @override
+  Future<void> editTransaction(TransactionModel model) async {
+    final transactionDB =
+        await Hive.openBox<TransactionModel>(TRANSACTION_DB_NAME);
+    await transactionDB.put(model.id, model);
+    refresh();
+  }
+
+  Future<void> search(String text) async {
+    final transactionDB =
+        await Hive.openBox<TransactionModel>(TRANSACTION_DB_NAME);
+
+    transactionList.clear();
+    transactionList.addAll(transactionDB.values
+        .where((element) => element.discription.contains(text)));
+    notifyListeners();
+  }
+}
